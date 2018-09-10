dc470d8932eafb17c3aa59b4cdd3260dcc05efdf
Przemysław Gierski <pgierski@o2.pl>
2018-09-10 14:38:24 +0200

    fix postinstall to avoid loop

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# here we check if this script was not yet executed, if it's a first run, then install this package dependecies
+# otherwise we would get into an infinite loop
+if [ ! -f "../../serverless.yml" ]
+then
+	npm install
+fi
+
 cp serverlessExample.yml ../../serverless.yml
-npm install --ignore-scripts
